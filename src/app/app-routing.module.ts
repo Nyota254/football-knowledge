0edce79928ce3210fde9ca1c975e93029c42593d
Nyota254 <knyota66@gmail.com>
2019-12-07 11:46:47 +0300

    add routing functionality to application

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { AboutComponent } from './about/about.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'' , redirectTo:'/home' , pathMatch:'full' },
+  { path:'home' , component:LandingPageComponent },
+  { path:'about' , component:AboutComponent },
+  { path:'**' , component:PageNotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { SearchFormsComponent } from './search-forms/search-forms.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { AboutComponent } from './about/about.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LandingPageComponent,
     SearchFormsComponent,
-    NavbarComponent
+    NavbarComponent,
+    AboutComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
